918889ec94ae7e5d15f7e3c61c7f565a91993336
SilverChu <536315064@qq.com>
2017-07-04 22:53:55 +0800

    Initial Commit

---
+++ StoreSearch/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  StoreSearch
+//
+//  Created by Silver Chu on 2017/7/4.
+//  Copyright © 2017年 Silver Chu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
