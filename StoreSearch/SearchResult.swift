142b5f9968ab77dc99b1a92cd7e527f0b6e1cc49
SilverChu <536315064@qq.com>
2017-07-05 16:29:34 +0800

    Add a search bar and table view. The search puts fake results in the table for now.

---
+++ StoreSearch/SearchResult.swift
@@ -0,0 +1,14 @@
+//
+//  SearchResult.swift
+//  StoreSearch
+//
+//  Created by Silver Chu on 2017/7/5.
+//  Copyright © 2017年 Silver Chu. All rights reserved.
+//
+
+import Foundation
+
+class SearchResult {
+    var name = ""
+    var artistName = ""
+}
