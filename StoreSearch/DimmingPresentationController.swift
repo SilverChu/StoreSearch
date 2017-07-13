75a7f5c9b96d596f5de4437e82a3255ddee340b4
SilverChu <536315064@qq.com>
2017-07-13 11:55:03 +0800

    Finish the work of detail pop-up view.

---
+++ StoreSearch/DimmingPresentationController.swift
@@ -0,0 +1,15 @@
+//
+//  DimmingPresentationController.swift
+//  StoreSearch
+//
+//  Created by Silver Chu on 2017/7/12.
+//  Copyright © 2017年 Silver Chu. All rights reserved.
+//
+
+import UIKit
+
+class DimmingPresentationController: UIPresentationController {
+    override var shouldRemovePresentersView: Bool {
+        return false
+    }
+}
--- StoreSearch/SearchResult.swift
+++ StoreSearch/SearchResult.swift
@@ -9,6 +9,7 @@
 import Foundation
 
 class SearchResult {
+    
     var name = ""
     var artistName = ""
     var artworkSmallURL = ""
@@ -18,6 +19,22 @@ class SearchResult {
     var currency = ""
     var price = 0.0
     var genre = ""
+    
+    func kindForDisplay() -> String {
+        switch kind {
+        case "album": return "Album"
+        case "audiobook": return "Audio Book"
+        case "book": return "Book"
+        case "feature-movie": return "Movie"
+        case "music-video": return "Music Video"
+        case "podcast": return "Podcast"
+        case "software": return "App"
+        case "song": return "Song"
+        case "tv-episode": return "TV Episode"
+        default: return kind
+        }
+    }
+    
 }
 
 func < (lhs: SearchResult, rhs: SearchResult) -> Bool {
